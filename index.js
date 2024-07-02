4e206224393f651ccf81123a7b917703fcf0d846
kasimkkn <kasikkn15@gmail.com>
2024-07-02 12:35:49 +0530

    done

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const {Server} = require('socket.io');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server, {
+  path: '/api/socket.io',
+});
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the chat page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected, socket.id = ' + socket.id);
+  console.log('a user connected, socket.id = ' + socket.id);
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
